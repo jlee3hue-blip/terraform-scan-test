41074ba81d077e2c0227a0aecb25ceec44a4fb72
jlee3hue-blip <jlee3hue@gmail.com>
2026-03-11 10:10:26 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "open_ssh" {
+  name = "aivric-test-open-ssh"
+
+  ingress {
+    description = "Open SSH to world"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
